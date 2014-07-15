262a792ba1fdcbfa7113c3adb596d5f3585af088
raju <raju@proisc.com>
2014-07-15 13:13:11 +0530

    Updated the following files to modified the footer design

--- application/views/header_footer/footer.php
+++ application/views/header_footer/footer.php
@@ -1,4 +1,56 @@
-<!-- FOOTER WRAPPER -->   <footer>
+</div>  <!-- Wrapper END -->    
+<div class="footer">
+	<div class="foot-width">
+		<div class="col-lft">
+			<div class="board">
+				<h3>Bulletin Board</h3>
+			</div>
+			<p>If you encounter a flight change or cancellation, please contact the appropriate airline. Flights for you to make sure you, call the airline to confirm return again, you can call us to confirm your departure flight in three days prior to departure.</p><br />
+			<p>Airline contact numbers (domestic) <br />
+			Air: 95583; CEA: 95530; <br />
+			Southern: 95539; HNA: 950718; <br />
+			Xiamen Airlines: 95557;</p>
+		 </div>
+		<div class="col-cen">
+			<div class="board">
+			<h3>Links</h3>
+			</div>
+			<p class="link-list">
+			> Lianhe Zaobao<br />
+			> 12306 China Railway Customer Service<br />
+			> Weather Forecast<br />
+			> Singapore Immigration<br />
+			> Singapore Ministry of Manpower
+			</p>
+		<span class="social-media">
+			<ul class="media-icon">
+				<li><img src="<?php echo base_url(); ?>assets/images/images/fbicon.jpg" /></li>
+				<li><img src="<?php echo base_url(); ?>assets/images/images/twico.jpg" /></li>
+				<li><img src="<?php echo base_url(); ?>assets/images/images/googleico.jpg" /></li>
+				<li><img src="<?php echo base_url(); ?>assets/images/images/inico.jpg" /></li>
+			</ul>
+		</span>
+		</div>
+		<div class="col-rgt">
+			<div class="board">
+			<h3>Consultation</h3>
+			</div>
+			<p>Email Address: satgurutravel@gmail.com</p>
+			<span class="feedback">
+			<h2>FEEDBACk</h2>
+			<input class="plus" type="text" value="&nbsp;&nbsp;Email Address" />
+			</span>
+			<img src="<?php echo base_url(); ?>assets/images/images/logo-slide.jpg" style="margin-top:15px; " />
+			
+		</div>
+	</div>
+</div>
+<div class="foot-rigths">
+	<P style=" ">Copyright@satguru Travel & Tours. All Right Reserved</P>
+	<p style=" padding-left:65px; color:#3f9cd5; font-weight:bold;">Terms&Conditions | Disclaimer | Privacy Policy</p>
+	<p style=" padding-left:45px;">Designed and Developed By : DSS</p>
+</div>
+<!-- FOOTER WRAPPER    <footer>
     <div class="footer_wrapper">
         <div class="inner_wrapper">
             <div class="wid320 fleft top10">
@@ -8,7 +60,7 @@
                      <p>
 
 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five </p>
-                </div>  <div class="top30"><img src="<?php echo base_url(); ?>assets/images/payment.png" /></div>
+                </div>  <div class="top30"><img src="<?php //echo base_url(); ?>assets/images/payment.png" /></div>
                 <div></div>
             </div>
             <div class="wid320 fleft top10 left60">
@@ -25,21 +77,21 @@ Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, wh
                 <div class="top10" style="color:#fff; font-size:13px; line-height:17px;">Enter your email address below to receive our monthly fun-filled newsletter.</div>	
                 <div class="footer_header top30" style="font-weight:bold; font-size:13px;">Share with us</div>
                 <div class="top10">
-                    <span><a href="#" target="_blank"><img src="<?php echo base_url(); ?>assets/images/share/facebook.png" width="30" border="0" /></a></span>
+                    <span><a href="#" target="_blank"><img src="<?php //echo base_url(); ?>assets/images/share/facebook.png" width="30" border="0" /></a></span>
                     
-                    <span class="left20"><a href="#" target="_blank"><img src="<?php echo base_url(); ?>assets/images/share/twitter.png" width="30" border="0" /></a></span>
+                    <span class="left20"><a href="#" target="_blank"><img src="<?php //echo base_url(); ?>assets/images/share/twitter.png" width="30" border="0" /></a></span>
                     
-                    <span class="left20"><img src="<?php echo base_url(); ?>assets/images/share/google.png" width="30" /></span>
-                    <span class="left20"><img src="<?php echo base_url(); ?>assets/images/share/diggit.png" width="30" /></span>
-                    <span class="left20"><img src="<?php echo base_url(); ?>assets/images/share/reddit.png" width="30" /></span>
-                    <span class="left20"><img src="<?php echo base_url(); ?>assets/images/share/linkedin.png" width="30" /></span>
+                    <span class="left20"><img src="<?php //echo base_url(); ?>assets/images/share/google.png" width="30" /></span>
+                    <span class="left20"><img src="<?php //echo base_url(); ?>assets/images/share/diggit.png" width="30" /></span>
+                    <span class="left20"><img src="<?php //echo base_url(); ?>assets/images/share/reddit.png" width="30" /></span>
+                    <span class="left20"><img src="<?php //echo base_url(); ?>assets/images/share/linkedin.png" width="30" /></span>
 
-<div class="top10"><span><img src="<?php echo base_url(); ?>assets/images/share/stumbleupon.png" width="30" /></span>
-                        <span class="left20"><img src="<?php echo base_url(); ?>assets/images/share/email.png" width="30" /></span>
-                        <span class="left20"><img src="<?php echo base_url(); ?>assets/images/share/pinterest.png" width="30" /></span>
-                        <span class="left20"><img src="<?php echo base_url(); ?>assets/images/share/bookmark.png" width="30" /></span>
-                        <span class="left20"><img src="<?php echo base_url(); ?>assets/images/share/orkut.png" width="30" /></span>
-                        <span class="left20"><img src="<?php echo base_url(); ?>assets/images/share/blogger.png" width="30" /></span></div>
+<div class="top10"><span><img src="<?php //echo base_url(); ?>assets/images/share/stumbleupon.png" width="30" /></span>
+                        <span class="left20"><img src="<?php //echo base_url(); ?>assets/images/share/email.png" width="30" /></span>
+                        <span class="left20"><img src="<?php //echo base_url(); ?>assets/images/share/pinterest.png" width="30" /></span>
+                        <span class="left20"><img src="<?php //echo base_url(); ?>assets/images/share/bookmark.png" width="30" /></span>
+                        <span class="left20"><img src="<?php //echo base_url(); ?>assets/images/share/orkut.png" width="30" /></span>
+                        <span class="left20"><img src="<?php //echo base_url(); ?>assets/images/share/blogger.png" width="30" /></span></div>
 
                 </div>
 
@@ -48,8 +100,8 @@ Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, wh
             <div class="wid200 fleft top10" >
                 <div class="footer_header left60 top10" style="font-weight:bold; font-size:13px;">Links</div>
                 <div class="footer_txt left40">	<ul>
-                        <li><a href="<?php echo site_url(); ?>/home/hotels"> Hotels </a></li>
-                        <li><a href="<?php echo site_url(); ?>/home/flights">Flights </a></li>
+                        <li><a href="<?php //echo site_url(); ?>/home/hotels"> Hotels </a></li>
+                        <li><a href="<?php //echo site_url(); ?>/home/flights">Flights </a></li>
                         <li><a href="#">Packages </a></li>
                         <li><a href="#">Events </a></li>
                         <li><a href="#">FAQ's </a></li>
@@ -65,13 +117,13 @@ Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, wh
         <div class="clear"></div>
         <div style="background-color:#277eb9; line-height:40px; width:100%; float:left; color:#FFF; " >
             <div class="inner_wrapper">
-                <div  class="fleft">@ <?php echo date('Y')?> DSS.com All Rights Reserved</div>
+                <div  class="fleft">@ <?php // echo date('Y')?> DSS.com All Rights Reserved</div>
                 <div class="fright">powered by DSS</div>
             </div>                                            
         </div></div>
 
 </footer> <!-- FOOTER WRAPPER END --> 
-</div>  <!-- Wrapper END -->    
+
 <script type="text/javascript">
 function check_new_sub(email)
 {

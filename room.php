a83ac56376d5efc4459add27d8c2633c8a1650e0
megamind6 <megamind6@megamind6-laptop.(none)>
2014-08-01 16:59:14 +0530

    Updated the following files to added the discount rate on hotel details page

--- room.php
+++ room.php
@@ -54,9 +54,82 @@
 				$arr .=	'<tr align="center" id="" class="table_rate">
 							<td align="left" id="" class="rm_promotion"><br><b>'.$hotelRoomRow['RoomName'].'</b></td>
 							<td id="" class="rm_book"><br><a href="'.$link.$hotelRoomRow['id'].'" class=""><span class="book_new_btn">Book</span></a></td>';
-						for($rc=1; $rc<=$_SESSION['room_count']; $rc++) {
+						for($rc=0; $rc<$_SESSION['room_count']; $rc++) {
 									$show_b_date = date_create($_SESSION['sd']);
-									for($i_d=1; $i_d<=$_SESSION['days']; $i_d++) {
+									for($i_d=0; $i_d<$_SESSION['days']; $i_d++) {
+		$date_val[] = date('Y-m-d', strtotime($_SESSION['hotel_search']['cin']. " + $i_d days"));
+		$newdate = date('Y-m-d', strtotime($_SESSION['hotel_search']['cin']. " + $i_d days"));
+		$day_of_the_week = date('l', strtotime($newdate));
+		$day_of_the_week1 = date('w', strtotime($newdate));
+		//Currency
+		// Price
+		$roomprice = mysql_query("select * from hotel_room_price where '".$newdate."' between ratefrom and rateto  and HotelCode='".$hotelIds."' and Roomcode='".$hotelRoomRow['RoomCode']."'");
+		$fetch_price = mysql_fetch_array($roomprice);
+		$contractrate = $fetch_price['contractrate'];
+		$roompricemarkup = $fetch_price['roompricemarkup'];
+		$surcharge = $fetch_price['surcharge'];
+		$weekdayfrom = $fetch_price['weekdayfrom'];
+		$weekdaytill = $fetch_price['weekdaytill'];
+		//holidaySurcharge
+		$holidaysurcharge = mysql_query("select ratetosurcharge from hotel_room_holidayprice where HotelCode='".$hotelIds."' and Roomcode='".$hotelRoomRow['RoomCode']."' and ratefromh='".$newdate."'");
+		$fetch_holidayprice = mysql_fetch_array($holidaysurcharge);
+		$fetch_holidaysur = $fetch_holidayprice['ratetosurcharge'];
+		//Total Price
+		$totalpricemarkup = $contractrate + $roompricemarkup;
+		$totalprice = $contractrate + $roompricemarkup + $fetch_holidaysur;
+		// Total Price Discount
+		$discountrate = mysql_query("select discountrate from  hotel_price_discount where '".$newdate."' between discountfrom and  discountto  and HotelCode='".$hotelIds."' and Roomcode='".$hotelRoomRow['RoomCode']."'");
+		$numfetchdiscount = mysql_num_rows($discountrate);
+		$fetchdiscount = mysql_fetch_array($discountrate);
+		$discountrate=$fetchdiscount['discountrate'];
+		if($numfetchdiscount>0)
+		{
+			$totalprice1 = $totalprice/$discountrate;
+			$totalprice = $totalprice-$totalprice1;
+		}
+		// hotel_roompricediscount
+		$roomprice = mysql_query("select pricerate from  hotel_roompricediscount where '".$newdate."' between pricefrom and  priceto  and HotelCode='".$hotelIds."' and Roomcode='".$hotelRoomRow['RoomCode']."'");
+		$numfetchpricediscount = mysql_num_rows($roomprice);
+		$fetchdiscountprice = mysql_fetch_array($roomprice);
+		$discountpricerate = $fetchdiscountprice['pricerate'];
+		if($numfetchpricediscount>0)
+		{
+			$totalprice = $totalprice-$discountpricerate;
+		}
+		// Pay stay Promotion
+		$paystaypromotion = mysql_query("select * from hotel_paystaypromo where '".$newdate."' between ratefrom and rateto and HotelCode='".$hotelIds."' and Roomcode='".$hotelRoomRow['RoomCode']."'");
+		$fetch_holidaypromotion=mysql_fetch_array($paystaypromotion);
+		$fetch_stay = $fetch_holidaypromotion['stay'];
+		$breakfast = $fetch_holidaypromotion['breakfast'];
+		if($days >= $fetch_stay) 
+		{
+			$fetch_pay = $fetch_holidaypromotion['pay'];
+			$differenc = $fetch_stay-$fetch_pay;
+			$differencmarkup = $differenc*$totalpricemarkup;
+			if($breakfast == 'yes')
+			{
+				$breakrate = $fetch_holidaypromotion['breakrate'];
+				$breakmarkup = $fetch_holidaypromotion['breakmarkup'];
+				$totalbreak = $breakrate + $breakmarkup;
+			}
+		}
+		// Weekend promo
+		$weekdaysurcharge = mysql_query("select * from hotel_priceweekendpromo where '".$newdate."' between ratefrom and rateto and '".$day_of_the_week1."' between weekdayfrom and weekdaytill and  HotelCode='".$hotelIds."' and Roomcode='".$hotelRoomRow['RoomCode']."'");
+		if ($weekdaysurcharge)
+		{
+			$fetch_weekdaysurcharge = mysql_fetch_array($weekdaysurcharge);
+			$weekdayfrom = $fetch_weekendpromo['weekdayfrom'];
+			$weekdaytill = $fetch_weekendpromo['weekdaytill'];
+			$weekendrate = $fetch_weekendpromo['weekendrate'];
+			$totalprice2 = $totalprice/$discountpricerate;
+			$totalprice = $totalprice-$totalprice2;
+		}
+		// Weekday surcharge
+		$weekendpromo = mysql_query("select * from hotel_room_price where '".$newdate."' between ratefrom and rateto and '".$day_of_the_week1."' between weekdayfrom and weekdaytill  and HotelCode='".$hotelIds."' and Roomcode='".$hotelRoomRow['RoomCode']."'");
+		$fetch_weekendpromo = mysql_fetch_array($weekendpromo);
+		$weeksurcharge = $fetch_weekendpromo['surcharge'];
+		$totalprice = $totalprice + $weeksurcharge;
+		$hotelRoomRow['AvgPrice'] = $totalprice;
 										$arr.= '<td class="rate_Instant"><br>
 										<span id="" class="oldrate"><input type="hidden" value="" id="price1_org'.$pr_c.'"><span id="price1_'.$pr_c.'"></span></span>
 										<input type="hidden"  id="price_org'.$pr_c.'" value="'.$hotelRoomRow['AvgPrice'].'" ><span id="price'.$pr_c.'">'.$hotelRoomRow['AvgPrice'].'</span></td>';
